600697c5cd63db7a385885ac257cbb8f077a69dd
Oscar Rubio <ofrubio0@gmail.com>
2024-04-15 21:02:52 -0500

    (ADD) Se crea el componente TextMessageBoxSelect

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react"
-import { GptMessage, MyMessage, TextMessageBox, TextMessageBoxFile, TypingLoader } from "../../components"
+import { GptMessage, MyMessage, TextMessageBox, TypingLoader } from "../../components"
 
 interface Message{
   text: string
@@ -49,16 +49,12 @@ export const OrthographyPage = () => {
 
         </div>
       </div>
-{/*       <TextMessageBox
+      <TextMessageBox
         onSendMessage={handlePost}
         placeholder="Escribe aqui lo que deseas"
         disbleCorrections
-      /> */}
-
-      <TextMessageBoxFile
-        onSendMessage={handlePost}
-        placeholder="Escribe aqui lo que deseas"
       />
+
     </div>
   )
 }
