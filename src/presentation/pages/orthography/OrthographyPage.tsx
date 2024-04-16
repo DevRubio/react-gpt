12fe1d5ff599a6c3b88ff4ada736f701d7a1d4be
Oscar Rubio <ofrubio0@gmail.com>
2024-04-15 20:11:59 -0500

    (UPDATE) Se agregan las funcionalidades de los mensajes

--- src/presentation/pages/orthography/OrthographyPage.tsx
+++ src/presentation/pages/orthography/OrthographyPage.tsx
@@ -1,6 +1,24 @@
+import { useState } from "react"
 import { GptMessage, MyMessage, TextMessageBox, TypingLoader } from "../../components"
 
+interface Message{
+  text: string
+  isGpt: boolean
+}
+
 export const OrthographyPage = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [messages, setMessages] = useState<Message[]>([])
+
+  const handlePost = async(text: string)=>{
+    setIsLoading(true)
+    setMessages((prev)=>[...prev,{text: text, isGpt: false}])
+
+    //Use Case   
+
+    setIsLoading(false)
+
+  }
   return (
     <div className="chat-container">
       <div className="chat-messages">
@@ -9,18 +27,30 @@ export const OrthographyPage = () => {
 
           {/*Bienvenida */}
           <GptMessage text="Hola, puedes escribir tu texto en español y te ayudo con las correciones"/>
-
-          <MyMessage text="Hola Mundo "/>
-
-          <TypingLoader className="fade-in"/>
-
-
-
+          {
+            messages.map((message, index)=>(
+              message.isGpt
+                ?(
+                  <GptMessage key={index} text="Esto es de OpenAi"/>
+                )
+                :(
+                  <MyMessage key={index} text={message.text}/>
+                )
+                
+            ))
+          }
+          {
+            isLoading &&(
+          <div className="col-start-1 col-end-12 fade-in">
+             <TypingLoader className="fade-in"/>
+          </div>
+            )
+          }
 
         </div>
       </div>
       <TextMessageBox
-        onSendMessage={(message) =>console.log(message)}
+        onSendMessage={handlePost}
         placeholder="Escribe aqui lo que deseas"
         disbleCorrections
       />
