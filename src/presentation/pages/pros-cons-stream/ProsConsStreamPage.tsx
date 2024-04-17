930e118e7d28abfc368111fd8508a4d691658ac2
Oscar Rubio <ofrubio0@gmail.com>
2024-04-17 18:23:31 -0500

    (ADD) Se crea el prosConsStreamUseCase y se integra a ProsConsStreamPage

--- src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
+++ src/presentation/pages/pros-cons-stream/ProsConsStreamPage.tsx
@@ -1,7 +1,73 @@
+import { useState } from "react"
+import { GptMessage, MyMessage, TextMessageBox, TypingLoader } from "../../components"
+import { prosConsStreamUseCase } from "../../../core/use-cases"
 
+interface Message{
+  text: string
+  isgpt: boolean
+}
 
 export const ProsConsStreamPage = () => {
+  const [isLoading, setIsLoading] = useState(false)
+  const [messages, setMessages] = useState<Message[]>([])
+
+  const handlePost = async(text:string)=>{
+    setIsLoading(true)
+    setMessages((prev)=>[...prev,{text: text, isgpt: false}])
+
+    const reader = await prosConsStreamUseCase(text)
+    setIsLoading(false)
+    if(!reader) return alert('No se pudo generar el reader')
+
+    const decoder = new TextDecoder()
+    let message = ''
+    setMessages((messages) =>[...messages, {text: message, isgpt: true}])
+
+    while(true){
+
+      const {value, done} = await reader.read()
+      if(done) break
+      const decodedChunk = decoder.decode(value, {stream: true})
+      message += decodedChunk
+
+      setMessages((messages)=>{
+        const newMessages = [...messages]
+        newMessages[newMessages.length-1].text = message
+        return newMessages
+      })
+    }
+
+
+  }
   return (
-    <div>ProsConsStreamPage</div>
+    <div className="chat-container">
+      <div className="chat-messages">
+        <div className="grid grid-cols-12 gap-y-2">
+          {/*Bienvenida */}
+          <GptMessage text="¿Que deseas comparar hoy?"/>
+          {
+            messages.map((message, index)=>(
+              message.isgpt
+                ?(
+                  <GptMessage key={index} text={message.text}/>
+                )
+                :(
+                  <MyMessage key={index} text={message.text}/>
+                )
+            ))
+          }{
+            isLoading &&(
+              <div className="col-start-1 col-end-12 fade-in">
+                <TypingLoader className="fade-in"/>
+              </div>
+            )
+          }
+        </div>
+      </div>
+      <TextMessageBox
+        onSendMessage={handlePost}
+        placeholder="¿Que deseas comparar hoy?"        
+      />
+    </div>
   )
 }
